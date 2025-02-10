f51e20799ac185019f999d1632489337591f98e3
MahmoudBadawy0 <mahmoudbadawy406@gmail.com>
2025-02-10 23:41:24 +0200

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  isLoading: boolean = false;
+  msgError: string = '';
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/),
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^(?=.*[A-Z])(?=.*[\W_]).{8,}$/),
+    ]),
+  });
+
+  // { updateOn: 'submit' }
+
+  submitLogin() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+
+      this.authService.sendLoginForm(this.loginForm.value).subscribe({
+        next: (res) => {
+          if (res.message === 'success') {
+            setTimeout(() => {
+              this.router.navigate(['/home']);
+            }, 500);
+          }
+          this.isLoading = false;
+        },
+        error: (err: HttpErrorResponse) => {
+          this.msgError = err.error.message;
+          this.isLoading = false;
+        },
+      });
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  isLoading: boolean = false;
+  msgError: string = '';
+
+  registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[\W_]).{8,}$/),
+      ]),
+      rePassword: new FormControl(null),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^01[0125][0-9]{8}$/),
+      ]),
+    },
+    { validators: this.confirmPassword }
+  );
+
+  // { updateOn: 'submit' }
+
+  submitRegister() {
+    if (this.registerForm.valid) {
+      this.isLoading = true;
+
+      this.authService.sendRegisterForm(this.registerForm.value).subscribe({
+        next: (res) => {
+          if (res.message === 'success') {
+            setTimeout(() => {
+              this.router.navigate(['/login']);
+            }, 500);
+          }
+          this.isLoading = false;
+        },
+        error: (err: HttpErrorResponse) => {
+          this.msgError = err.error.message;
+          this.isLoading = false;
+        },
+      });
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  confirmPassword(group: AbstractControl) {
+    const password = group.get('password')?.value;
+    const rePassword = group.get('rePassword')?.value;
+    return password === rePassword ? null : { mismatch: true };
+  }
 }
